54b9f3aec40fdc5c9abd629c68b0f6e1663404e2
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-05 17:58:50 -0500

    func wrap

--- script.js
+++ script.js
@@ -3,34 +3,41 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
-if (listItemsArray.length > 0) {
-  const filteredArray = listItemsArray.filter(el => {
-    let content = "";
-    if (inputVal.includes(",")) {
-      if (inputVal.split(",")[1].length > 2) {
-        inputVal = inputVal.split(",")[0];
-        content = el
-          .querySelector(".city-name span")
-          .textContent.toLowerCase();
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      if (inputVal.includes(",")) {
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
       } else {
-        content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
       }
-    } else {
-      content = el.querySelector(".city-name span").textContent.toLowerCase();
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already checked the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } provide country code`;
+      form.reset();
+      input.focus();
+      return;
     }
-    return content == inputVal.toLowerCase();
-  });
-
-  if (filteredArray.length > 0) {
-    msg.textContent = `You already checked the weather for ${
-      filteredArray[0].querySelector(".city-name span").textContent
-    } provide country code`;
-    form.reset();
-    input.focus();
-    return;
   }
-
 
   msg.textContent = "";
   form.reset();
   input.focus();
+});
