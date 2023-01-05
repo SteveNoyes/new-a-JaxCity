f0fa1a75ad4bdacee6b8dbcc975f6a7e24b761f8
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-05 17:54:41 -0500

    check for current city name, if any

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
+
+if (listItemsArray.length > 0) {
+  const filteredArray = listItemsArray.filter(el => {
+    let content = "";
+    if (inputVal.includes(",")) {
+      if (inputVal.split(",")[1].length > 2) {
+        inputVal = inputVal.split(",")[0];
+        content = el
+          .querySelector(".city-name span")
+          .textContent.toLowerCase();
+      } else {
+        content = el.querySelector(".city-name").dataset.name.toLowerCase();
+      }
+    } else {
+      content = el.querySelector(".city-name span").textContent.toLowerCase();
+    }
+    return content == inputVal.toLowerCase();
+  });
