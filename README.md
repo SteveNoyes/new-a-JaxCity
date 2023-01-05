955073900923ecc1ebb7e622833e6b4e7361c6df
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-05 17:53:05 -0500

    project so far

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+## Table of contents
+
+- [Overview](#overview)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+## Overview
+
 Weather Center API
+
+## My process
+
+### Built with
+
+- HTML
+- CSS  
+- JavaScript
+- Ajax
---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
